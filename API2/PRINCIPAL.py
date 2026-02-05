eb6d63df8eef177e09b8c8a6b2de1b4a971582f4
Josue Marin <marinjosue036@gmail.com>
2026-02-05 08:26:55 -0500

    Refactor employee database operations in PRINCIPAL.py

--- API2/PRINCIPAL.py
+++ API2/PRINCIPAL.py
@@ -1,4 +1,4 @@
-from Base_datos_empleado import Base_datos_empleado
+from base_datos import Base_datos_empleado
 from empleado_modelo import Empleado_modelo
 
   
@@ -17,11 +17,8 @@ objeto_empleado3= Empleado_modelo("Luis","Martinez","11223344","555-8765")
 lista_nuevos_empleados =(objeto_empleado,objeto_empleado2,objeto_empleado3)
 #llamo el metodo de la base de datos que guarda al objeto
 
-objeto_Api.agregar_empleado(objeto_empleado)
-objeto_Api.agregar_empleado(objeto_empleado2)
-objeto_Api.agregar_empleado(objeto_empleado3)
+objeto_Api.guardar_varios_empleados(lista_nuevos_empleados)
 
-objeto_Api.extender_varios_empleados(lista_nuevos_empleados)
 
 #imprimo toda la lista de empleados
 objeto_Api.imprimir_info()
